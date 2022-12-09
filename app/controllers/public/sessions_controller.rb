e800a94d8659bb2a5a6164e0fd612df7acd4832e
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-12-09 12:59:19 +0000

    [Create]users_is_deleted&my_page_profile_image

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :user_state, only[:create]
+  before_action :user_state, only:[:create]
   before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
@@ -28,6 +28,16 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   protected
+
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && !(@user.is_deleted == false)
+      flash[:notice] = "退会済みです。再度ご登録してご利用ください。"
+      redirect_to new_user_registration_path
+      # createアクションを実行する
+    end
+  end
 
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in,keys:[:email, :password])
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,6 +7,30 @@ class Public::UsersController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "会員情報の編集が完了しました"
+      redirect_to public_users_my_page_path(@user)
+    else
+      render :edit
+    end
+  end
+
   def unsubscribe
+    @user = current_user
+  end
+
+  def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました"
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :purpose, :email, :is_deleted)
   end
 end
