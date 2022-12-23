ba8ed65b4a42596765a9c0c8140096fea606aee4
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-12-23 14:56:19 +0000

    [update]admin_users_show&edit,to_do_posts_index(list)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,20 @@ class Admin::UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "会員情報の編集が完了しました"
+      redirect_to admin_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :purpose, :email, :is_deleted)
+  end
+
 end
